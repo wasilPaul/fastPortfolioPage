03bd66ff14d28b049902a5cece77f178a74f3489
Pawel Wasil <wasil.paul@gmail.com>
2018-04-09 01:07:58 +0200

    jQuery scrolling script added, navigation finish

--- scripts/nav_scrool.js
+++ scripts/nav_scrool.js
@@ -1,9 +1,23 @@
-$(document).ready(function() {
-        $(".contact").click(function () {
-            var offset = 0; //Offset of 00px
+$(document).ready(function () {
+    $(".nav-brand--style").click(function () {
+        var offset = 0; //Offset of 00px
 
-            $('html, body').animate({
-                scrollTop: $(".about_me").offset().top + offset
-            }, 2000);
-        });
+        $('html, body').animate({
+            scrollTop: $("#up_top").offset().top + offset
+        }, 2000);
+    });
+    $(".contact").click(function () {
+        var offset = 0;
+
+        $('html, body').animate({
+            scrollTop: $(".about_me").offset().top + offset
+        }, 2000);
+    });
+    $(".project").click(function () {
+        var offset = 0;
+
+        $('html, body').animate({
+            scrollTop: $(".projects").offset().top + offset
+        }, 2000);
+    });
 });
